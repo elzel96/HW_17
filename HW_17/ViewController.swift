1d587a0a94e141e0dac7598d31631b0bd1465467
elzel <elzel666@gmail.com>
2023-09-23 22:05:42 +0300

    Initial Commit

---
+++ HW_17/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_17
+//
+//  Created by Helena on 23.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
